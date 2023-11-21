66e1860d0f3e3d4c2a5997e8bd799903d867f29d
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2023-11-21 21:45:32 +0100

    spring initializer and application.properties file added

---
+++ src/main/java/com/example/bambergBeverageBox/BambergBeverageBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.bambergBeverageBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambergBeverageBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambergBeverageBoxApplication.class, args);
+	}
+
+}
